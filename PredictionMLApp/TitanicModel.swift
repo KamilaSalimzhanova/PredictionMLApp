8c033b16a412d868d4de9a0d38b1c910024343a6
kamila <ksalimzh@gmail.com>
2025-04-04 19:03:04 +0500

    Core ml model and model for titanic

---
+++ PredictionMLApp/TitanicModel.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+struct TitanicModel: Identifiable {
+    let id = UUID()
+    var passengerClass: String // 1 2 3
+    var sex: String // male female
+    var age: Int // 22
+    var siblingsSpouses: Double // 1 0
+    var parentsChildren: Double  // 4 3 2
+    var fare: Double
+    var port: String
+    
+    static let passClass = ["First Class", "Second Class", "Third Class"]
+    static let ports = ["Cherbourg", "Queenstown", "Southampton"]
+    static let genders = ["male", "female"]
+    
+    var pClass: Int64 {
+        switch passengerClass {
+        case "First Class":
+            return 1
+        case "Second Class":
+            return 2
+        case "Third Class":
+            return 3
+        default:
+            return 0
+        }
+    }
+}
