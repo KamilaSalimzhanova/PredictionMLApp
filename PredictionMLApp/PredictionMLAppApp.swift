868c023bc2c90f77c59f855a2e774603a1b706f9
kamila <ksalimzh@gmail.com>
2025-04-04 18:35:49 +0500

    Initial Commit

---
+++ PredictionMLApp/PredictionMLAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PredictionMLAppApp.swift
+//  PredictionMLApp
+//
+//  Created by kamila on 04.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct PredictionMLAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
