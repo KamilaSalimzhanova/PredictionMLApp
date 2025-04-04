24809a4aa738487461e62c58b14dcfbb5262ce06
kamila <ksalimzh@gmail.com>
2025-04-04 19:27:08 +0500

    section view, form, navigation stack

--- PredictionMLApp/ContentView.swift
+++ PredictionMLApp/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var titanic: TitanicModel = .init(
+        passengerClass: "Second Class",
+        sex: "male",
+        age: 18,
+        siblingsSpouses: 2,
+        parentsChildren: 4,
+        fare: 5,
+        port: "Cherbourg")
+    @State private var survival: Bool? = nil
+    @State private var showAlert: Bool = false
+    @State private var survivalRate: Double = -1
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                Form {
+                    FormSectionView(text: "Passenger class", newView: "To do")
+                    FormSectionView(text: "Passenger class", newView: "To do")
+                    FormSectionView(text: "Passenger class", newView: "To do")
+                    FormSectionView(text: "Passenger class", newView: "To do")
+                    FormSectionView(text: "Passenger class", newView: "To do")
+                    FormSectionView(text: "Passenger class", newView: "To do")
+                    
+                    if showAlert {
+                        Text("show alert")
+                    }
+                }
+            }
+            .navigationTitle("Surviving model")
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Button(action: {
+                        
+                    }) {
+                        Text("Compute")
+                            .foregroundColor(.red)
+                            .bold()
+                            .opacity(showAlert ? 0 : 1)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
